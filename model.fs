9480c7a09e6e34f44941960c792c48684981b415
Octavio Rodríguez-Castañeda <octavio.rdz@outlook.com>
2024-12-11 22:42:10 -0700

    Temporary Commit at 12/11/2024, 10:42:10 PM
    Summary:
    
    Test Plan:

---
+++ model.fs
@@ -0,0 +1,28 @@
+
+
+type Part = {PartNumber: string}
+
+type PartEntry =
+    { Part: Part
+      Quantity: int }
+    
+
+type KitSet =
+    { KitId: string
+      LineItem: Set<PartEntry> }
+      
+type StaticKit =
+    { KitId: string
+      LineItem: Set<PartEntry> }
+      
+type EditableKit =
+    { KitId:string
+      Part:ResizeArray<PartEntry> }
+      
+type Kit = 
+    | AsBuilt of StaticKit
+    | SpeedKit of EditableKit
+    
+type PartLine =
+    | Part
+    | Kit
