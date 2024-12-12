f24d33234b052f1c197973d4a5a545234cf71535
Octavio Rodríguez Castañeda <octavio.rdz@outlook.com>
2024-12-11 22:08:33 -0700

    Temporary Commit at 12/11/2024, 9:35:40 PM
    Summary:
    
    Test Plan:
    
    [ghstack-poisoned]

---
+++ model.fs
@@ -0,0 +1,27 @@
+open System
+
+type Part = {PartNumber: string}
+
+type PartEntry =
+    { Part: Part
+      Quantity: int }
+    
+type KitSet =
+    { KitId: string
+      LineItem: Set<PartEntry> }
+      
+type StaticKit =
+    { KitId: string
+      LineItem: Set<PartEntry> }
+      
+type EditableKit =
+    { KitId:string
+      Part:ResizeArray<PartEntry> }
+      
+type Kit = 
+    | AsBuilt of StaticKit
+    | SpeedKit of EditableKit
+    
+type PartLine =
+    | Part
+    | Kit
